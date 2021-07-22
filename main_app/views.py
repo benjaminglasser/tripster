7e472458006f347fdd6e65e13e3aaf13cb283f83
Betty Reaney <bettyreaney@Bettys-MacBook-Pro.local>
2021-07-21 18:38:12 -0700

    sets up auth

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -7,3 +10,9 @@ def home(request):
 
 def about (request):
     return render(request, 'about.html')
+
+def login (request):
+    return render(request, 'login.html')
+
+def logout (request):
+    return render(request, 'logout.html')
