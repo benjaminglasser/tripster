3a58148b65dd883f4de0bfa54219a8443a0d69f8
Fernando Guimaraes <ferguimara@gmail.com>
2021-07-22 00:20:31 -0400

    trips model and views created

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Trip
 
 # Create your views here.
 def home(request):
@@ -11,6 +13,28 @@ def home(request):
 def about (request):
     return render(request, 'about.html')
 
+#trips:
+def trips_index(request):
+    trips = Trip.objects.all()
+    return render(request, 'trips/index.html', {'trips': trips})
+
+def trips_detail(request, trip_id):
+    trip = Trip.objects.get(id=trip_id)
+    return render(request, 'trips/detail.html', {'trip': trip})
+
+class TripCreate(CreateView):
+    model = Trip
+    fields=['name', 'start_date', 'end_date', 'start_location', 'end_location']
+
+class TripUpdate(UpdateView):
+    model = Trip
+    fields=['name', 'start_date', 'end_date', 'start_location', 'end_location']
+
+class TripDelete(DeleteView):
+    model = Trip
+    success_url = '/trips/'
+
+#auth:
 def login (request):
     return render(request, 'login.html')
 
