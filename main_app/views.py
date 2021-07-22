9c11fc3e99f882f6863cc55d13f7b02a3c64587e
ben glasser <ben@fantasticvoyage.com>
2021-07-22 15:29:07 -0700

    views updates

--- main_app/views.py
+++ main_app/views.py
@@ -35,9 +35,8 @@ class TripCreate(CreateView):
               'start_location', 'end_location']
 
     def form_valid(self, form):
-        # Assign the logged in user (self.request.user)
-        form.instance.user = self.request.user  # form.instance is the cat
-        # Let the CreateView do its job as usual
+
+        form.instance.user = self.request.user
         return super().form_valid(form)
 
 
