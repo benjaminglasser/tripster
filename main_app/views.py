eb3814622d336fcf06a9a4687f6804749ad08f46
Fernando Guimaraes <ferguimara@gmail.com>
2021-07-31 11:54:08 -0400

    fixed photo url

--- main_app/views.py
+++ main_app/views.py
@@ -136,8 +136,7 @@ def add_photo(request, stop_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
         s3 = boto3.client('s3')
-        key = uuid.uuid4().hex[:6] + \
-            photo_file.name[photo_file.name.rfind('.'):]
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
         try:
             s3.upload_fileobj(photo_file, BUCKET, key)
             url = f"{S3_BASE_URL}{BUCKET}/{key}"
